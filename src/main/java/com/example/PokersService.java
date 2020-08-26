cd25c583ae93467784a95653c424a97708ae47e2
HaloHan2017 <2332722908@qq.com>
2020-08-26 21:02:17 +0800

    [tdd-poker-hands][jerry,devin,andrea] feature: update comparePokers

--- src/main/java/com/example/PokersService.java
+++ src/main/java/com/example/PokersService.java
@@ -3,6 +3,8 @@ package com.example;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.example.PokerUtil.convertPoker;
 
@@ -17,12 +19,28 @@ public class PokersService {
     }
 
     public String comparePokers(List<Poker> blackPokers, List<Poker> whitePokers) {
+        String calculatedResult = getCalculateResultOfPokers(blackPokers);
         Integer maxPokerNumberOfBlack = convertPoker(getMaxPoker(blackPokers).getNumber());
         Integer maxPokerNumberOfWhite = convertPoker(getMaxPoker(whitePokers).getNumber());
         if (maxPokerNumberOfBlack > maxPokerNumberOfWhite) {
             return "Black wins. - with high card: " + map.get(maxPokerNumberOfBlack);
         }
         return "White wins. - with high card: " + map.get(maxPokerNumberOfWhite);
+    }
+
+    private String getCalculateResultOfPokers(List<Poker> pokers) {
+        Map<String, Long> map = pokers.stream().map(Poker::getNumber).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        int mapSize = map.values().size();
+        if (mapSize == 5) {
+            return "No Pair";
+        } else if (mapSize == 4) {// 一对
+            for (String key : map.keySet()) {
+                if (map.get(key).longValue() == 2) {
+                    return "With Pair" + key;
+                }
+            }
+        }
+        return "";
     }
 
     public Poker getMaxPoker(List<Poker> pokers) {
