dd35bac765659ff671b1af0fc0cb74db07393763
HaloHan2017 <1006653740@qq.com>
2020-08-27 23:35:53 +0800

    [tdd-poker-hands] [jerry] refactor : update comparePokers method

--- src/main/java/com/example/PokersService.java
+++ src/main/java/com/example/PokersService.java
@@ -3,8 +3,6 @@ package com.example;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static com.example.PokerUtil.convertPoker;
 
@@ -19,56 +17,28 @@ public class PokersService {
     }
 
     public String comparePokers(List<Poker> blackPokers, List<Poker> whitePokers) {
-        Integer result = getCalculateResultOfPokers(blackPokers,whitePokers);
         int priorityOfBlackPokers = getPriorityOfPokers(blackPokers);
         int priorityOfWhitePokers = getPriorityOfPokers(whitePokers);
-        if(priorityOfBlackPokers < priorityOfWhitePokers){
-            return "White wins.";
-        }else if(priorityOfBlackPokers > priorityOfWhitePokers){
-            return "Black wins.";
-        }else {
+        if (priorityOfBlackPokers == priorityOfWhitePokers) {
             // the same priority
-            return null;
-        }
-        if(result == 0){
             Integer maxPokerNumberOfBlack = convertPoker(getMaxPoker(blackPokers).getNumber());
             Integer maxPokerNumberOfWhite = convertPoker(getMaxPoker(whitePokers).getNumber());
             if (maxPokerNumberOfBlack > maxPokerNumberOfWhite) {
                 return "Black wins. - with high card: " + map.get(maxPokerNumberOfBlack);
             }
             return "White wins. - with high card: " + map.get(maxPokerNumberOfWhite);
-        }else if(result == 1){
+        } else if (priorityOfBlackPokers < priorityOfWhitePokers) {
             return "White wins.";
-        }else if (result == 2){
-            return "White wins.";
+        } else {
+            return null;
         }
-        return  "";
     }
 
     private int getPriorityOfPokers(List<Poker> pokers) {
-        if(PokerUtil.hasOnePair(pokers)){
+        if (PokerUtil.hasOnePair(pokers)) {
             return 1;
         }
         return 0;
-    }
-
-    private Integer getCalculateResultOfPokers(List<Poker> blackPokers, List<Poker> whitePokers) {
-        Map<String, Long> mapOfBlack = blackPokers.stream().map(Poker::getNumber).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        Map<String, Long> mapOfWhite = whitePokers.stream().map(Poker::getNumber).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        return judgeByMap(mapOfBlack, mapOfWhite);
-    }
-
-    private Integer judgeByMap(Map<String, Long> mapOfBlack, Map<String, Long> mapOfWhite) {
-        int mapOfBlackSize = mapOfBlack.values().size();
-        int mapOfWhiteSize = mapOfWhite.values().size();
-        if (mapOfBlackSize == 5 && mapOfWhiteSize == 5) { // 0对 vs 0对
-            return 0;
-        } else if (mapOfBlackSize == 5 && mapOfWhiteSize == 4) {// 0对 vs 1对
-            return 1;
-        }else if(mapOfBlackSize == 4 && mapOfWhiteSize == 3){// 1对 vs 2对
-            return 2;
-        }
-        return null;
     }
 
     public Poker getMaxPoker(List<Poker> pokers) {
